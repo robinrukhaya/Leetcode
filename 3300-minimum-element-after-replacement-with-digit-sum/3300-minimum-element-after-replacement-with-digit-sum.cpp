3eb7c48ef0ce325cdf5a5a9e10b44bb52801b8be
Robin <robinrukhaya54@gmail.com>
2026-05-29 20:41:53 +0530

    Time: 0 ms (100%), Space: 27.8 MB (23.8%) - LeetHub

---
+++ 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int digitSum(int num) {
+        int sum = 0;
+
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+
+        return sum;
+    }
+
+    int minElement(vector<int>& nums) {
+        int mini = INT_MAX;
+
+        for (int num : nums) {
+            mini = min(mini, digitSum(num));
+        }
+
+        return mini;
+    }
+};
